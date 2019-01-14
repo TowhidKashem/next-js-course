70e077f150ee90ea25ab74830f359c2c035e801f
TowhidKashem <tkashem@gmail.com>
2019-01-14 15:00:21 -0500

    work

---
+++ hacker-news-clone/pages/index.jsx
@@ -0,0 +1,59 @@
+import fetch from "isomorphic-unfetch";
+import Error from "next/error";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import StoryList from "../components/StoryList";
+
+export default class Index extends React.Component {
+  static async getInitialProps({ req, res, query }) {
+    let stories;
+    let page;
+
+    try {
+      page = Number(query.page) || 1;
+
+      const response = await fetch(
+        `https://node-hnapi.herokuapp.com/news?page=${page}`
+      );
+
+      stories = await response.json();
+    } catch (error) {
+      console.log(error);
+
+      stories = [];
+    }
+
+    return { stories, page };
+  }
+
+  render() {
+    const { stories, page } = this.props;
+
+    if (!stories.length) {
+      return <Error status={503} />;
+    }
+
+    return (
+      <Layout title="Hacker Clone" description="A hacker news clone">
+        <StoryList stories={stories} />
+
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+
+        <style jsx>{`
+          footer {
+            padding: 1em;
+          }
+          footer a {
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+          }
+        `}</style>
+      </Layout>
+    );
+  }
+}
